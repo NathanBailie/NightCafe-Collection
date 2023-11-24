ed131b067c0dd753124869a0a6830dc02b643068
Nathan <antnefedov33@gmail.com>
2023-11-24 17:01:17 +0300

    added scripts to make inactive images blured

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,20 +4,45 @@ let images = document.querySelectorAll('.item');
 
 for (let image of images) {
     image.addEventListener('click', () => {
-        resetClasses(images, image.id)
+        resetClassesFromTarget(images, image.id);
+        addInactiveClass(images, image.id);
 
         if (image.classList.contains('active')) {
-            image.classList.remove('active');
+            resetClasses(images)
         } else {
             image.classList.add('active');
         };
     });
 };
 
-function resetClasses(array, id) {
+function resetClassesFromTarget(array, id) {
     for (let item of array) {
         if (item.id !== id) {
             item.classList.remove('active');
+            item.classList.remove('filtered');
         };
     };
+}
+
+function resetClasses(array) {
+    for (let item of array) {
+        item.classList.remove('active');
+        item.classList.remove('filtered');
+    };
+}
+
+function addInactiveClass(array, id) {
+    for (let item of array) {
+        if (item.id !== id) {
+            item.classList.add('filtered');
+        }
+    }
+}
+
+function resetInactiveClass(array, id) {
+    for (let item of array) {
+        if (item.id !== id) {
+            item.classList.remove('filtered');
+        }
+    }
 }

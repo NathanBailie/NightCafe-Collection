4066a0edd70903a6bee8034cb7c2c71a05395bab
Nathan <antnefedov33@gmail.com>
2023-11-24 16:21:17 +0300

    some fixes in styles and added script to add active class to chosen image

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,23 @@
 "use strict"
+
+let images = document.querySelectorAll('.item');
+
+for (let image of images) {
+    image.addEventListener('click', () => {
+        resetClasses(images, image.id)
+
+        if (image.classList.contains('active')) {
+            image.classList.remove('active');
+        } else {
+            image.classList.add('active');
+        };
+    });
+};
+
+function resetClasses(array, id) {
+    for (let item of array) {
+        if (item.id !== id) {
+            item.classList.remove('active');
+        };
+    };
+}
